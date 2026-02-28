f946c5b196e0933a7702cfa35124a4867f25783b
mutheeew <muthmainnah133@gmail.com>
2026-03-01 05:43:41 +0700

    add project section versi mobile

--- components/sections/project-section.tsx
+++ components/sections/project-section.tsx
@@ -109,18 +109,21 @@ const ProjectCardMobile: React.FC<{title: string; icons: React.ReactNode[]; desc
       className="md:hidden bg-white rounded-lg shadow-lg p-6 transition-all hover:shadow-xl cursor-pointer w-full"
       onClick={() => link && window.open(link, '_blank')}
     >
-      <div className="mb-4">
+      <div className="flex mb-4 gap-4">
         <div className="w-12 h-12 bg-gray-200 rounded-full mb-4"></div>
+        <div className="">
+          <h2 className="text-xl font-bold tracking-tight uppercase">
+            {title.length > 15 ? title.slice(0, 20) + "..." : title}
+          </h2>
+          <div className="flex gap-2 text-sm font-semibold text-gray-700">
+            {icons.map((icon, i) => (
+              <span key={i}>{icon}</span>
+            ))}
+          </div>
+        </div>
       </div>
-      <h2 className="text-2xl font-bold mb-3 tracking-tight uppercase">
-        {title}
-      </h2>
-      <div className="flex gap-2 mb-3 text-lg font-semibold text-gray-700">
-        {icons.map((icon, i) => (
-          <span key={i}>{icon}</span>
-        ))}
-      </div>
-      <p className="text-sm text-gray-600 leading-relaxed">{description}</p>
+      
+      <p className="text-sm text-gray-600 leading-relaxed">{description.length > 100 ? description.slice(0, 90) + "..." : description}</p>
     </div>
   );
 }
@@ -203,7 +206,7 @@ const ProjectSection: React.FC = () => {
           <h2 className="text-6xl md:text-7xl font-bold mb-6 tracking-tight">
             SELECTED PROJECTS
           </h2>
-          <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+          <p className="text-lg sm:ext-xl text-gray-600 max-w-2xl mx-auto">
             Explore our diverse portfolio of innovative projects and creative solutions
           </p>
         </div>
